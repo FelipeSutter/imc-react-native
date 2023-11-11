65a005c38368304717f0e2ad2373de0381fcc5d7
Rian-Fernandes <rian10fernandes@hotmail.com>
2023-11-10 21:48:10 -0300

    First commit

--- calculo-imc/components/Main/index.js
+++ calculo-imc/components/Main/index.js
@@ -1,29 +1,30 @@
-import {
-  View,
-  Text,
-  TextInput,
-  Button,
-  FlatList,
-  StyleSheet,
-} from "react-native";
-import { useState } from "react";
+import React, { useState } from 'react';
+import { View, Text, TextInput, Button, StyleSheet } from 'react-native';
 
 const Main = () => {
-  const [peso, setPeso] = useState(0);
-  const [altura, setAltura] = useState(0);
-  const [imc, setImc] = useState();
-  const [resultado, setResultado] = useState();
+  const [peso, setPeso] = useState('');
+  const [altura, setAltura] = useState('');
+  const [imc, setImc] = useState(null);
+  const [diagnostico, setDiagnostico] = useState('');
 
   const calcularImc = () => {
-    if (peso == "" || altura == "") {
-      alert("Preencha os campos de peso e altura.");
+    if (peso === '' || altura === '') {
+      alert('Preencha os campos de peso e altura.');
     } else {
-      let calculoImc = peso / (altura * altura);
-      setImc(calculoImc);
+      const calculoImc = parseFloat(peso) / (parseFloat(altura) * parseFloat(altura));
+      setImc(calculoImc.toFixed(2));
+
+      if (calculoImc < 18.5) {
+        setDiagnostico(`Voce está abaixo do peso. Seu IMC é:`);
+      } else if (calculoImc < 24.9) {
+        setDiagnostico(`Seu peso está normal. Seu IMC é: `);
+      } else if (calculoImc < 29.9) {
+        setDiagnostico(`Voce está em sobrepeso. Seu IMC é:`);
+      } else {
+        setDiagnostico(`Voce está obeso. Seu IMC é:`);
+      }
     }
   };
-
-  const MostrarResultado = () => <Text>{imc}</Text>;
 
   return (
     <View style={styles.container}>
@@ -31,24 +32,28 @@ const Main = () => {
         value={peso}
         placeholder="Digite seu peso"
         onChangeText={(text) => setPeso(text)}
-      ></TextInput>
+        keyboardType="numeric"
+      />
       <TextInput
         value={altura}
         placeholder="Digite sua altura"
         onChangeText={(text) => setAltura(text)}
-      ></TextInput>
-      <Button title="Clique em mim" onPress={calcularImc}></Button>
-      <FlatList data={imc} renderItem={MostrarResultado}></FlatList>
+        keyboardType="numeric"
+      />
+      <Button title="Clique em mim" onPress={calcularImc} />
+      
+      {imc !== undefined && <Text>IMC: {imc}</Text>}
+      {diagnostico!== "" &&<Text>Diagnostico:{diagnostico}</Text>}
     </View>
   );
 };
-
 const styles = StyleSheet.create({
   container: {
-    justifyContent: "center",
-    alignItems: "center",
+    justifyContent: 'center',
+    alignItems: 'center',
     marginTop: 50,
   },
 });
 
 export default Main;
+  
